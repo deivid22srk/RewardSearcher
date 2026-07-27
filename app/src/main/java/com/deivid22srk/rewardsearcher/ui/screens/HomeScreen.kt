f0b647ac299878c31e6e8200ab85c2a8c5141f75
kardeiro <kardeiro@users.noreply.github.com>
2026-07-27 08:20:39 -0300

    fix: add missing onValueChangeFinished param to Slider

--- app/src/main/java/com/deivid22srk/rewardsearcher/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/deivid22srk/rewardsearcher/ui/screens/HomeScreen.kt
@@ -140,6 +140,7 @@ fun HomeScreen(
                 Slider(
                     value = count,
                     onValueChange = { count = it },
+                    onValueChangeFinished = {},
                     valueRange = 1f..100f,
                     steps = 98
                 )
